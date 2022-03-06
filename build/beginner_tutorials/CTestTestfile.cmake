57f4336c6f1614efffe000ffb572e878f6b57d72
JoseOlivas <chocheman7@hotmail.com>
2022-03-06 16:42:41 -0600

    Se agregó el pkg del tutorial beginner_tutorials

--- build/beginner_tutorials/CTestTestfile.cmake
+++ build/beginner_tutorials/CTestTestfile.cmake
@@ -1,8 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/nepptuno/camarasUS/src
-# Build directory: /home/nepptuno/camarasUS/build
+# Source directory: /home/nepptuno/camarasUS/src/beginner_tutorials
+# Build directory: /home/nepptuno/camarasUS/build/beginner_tutorials
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("beginner_tutorials")
