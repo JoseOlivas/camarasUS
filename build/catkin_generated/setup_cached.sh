54072bbfbc0f3617cc0a301d42518645d32aa93c
JoseOlivas <chocheman7@hotmail.com>
2022-03-06 19:33:28 -0600

    Se agregó rosserial

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -9,6 +9,8 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/nepptuno/camarasUS/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/nepptuno/camarasUS/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/nepptuno/camarasUS/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/nepptuno/camarasUS/build'
+export PYTHONPATH="/home/nepptuno/camarasUS/devel/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES='/home/nepptuno/camarasUS/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/nepptuno/camarasUS/src:$ROS_PACKAGE_PATH"
